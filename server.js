d1df0ea8f165f6d684cad7f47c2fb6e824e2f5cf
manikota <rammanikanta814@gmail.com>
2017-03-22 19:32:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
+	
+	var express=require('express');
+	var Pool=require ('pg').Pool;
 
-var app = express();
-app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+	var app=express();
+	var config={
+			
+			user:'manikota',		
+			database:'manikota',
+			host:'db.imad.hasura.io',
+			port:'5432',
+			password:process.env.DB_PASSWORD
+		}
+	
+	var pool=new Pool(config);
+	
+	app.get('/:cityname',function(req,res)
+				{
+					
+					pool.query("select * from weather where city=$1",[req.params.cityname],function(err,result)
+																													
+					{
+					
+						if(err)
+						
+							res.status(500).send("Cannot connect");
+		
+						else
+									
+							res.status(200).send(result.rows);
+					});
+				}
+		);
+	app.listen(3000);
